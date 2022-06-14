a73eaa713b54e72de3124ee6d0667cd87efecb4f
JosueAVD27 <ajvelasquez4@espe.edu.ec>
2022-06-14 02:13:09 -0500

    Version 10

--- APW.py
+++ APW.py
@@ -11,62 +11,92 @@ lista_sugerencias = []
 
 #Decorador para definir la ruta inicio
 @app.route('/')
+#Crea la funcion para llamar a la pagina de inicio
 def index():
     return render_template('index.html')
 
 #Decorador para definir la ruta contacto
 @app.route('/contacto')
+#Crea la funcion para llamar a la pagina de contacto
 def contacto():
     return render_template('contacto.html', lista_contactos = lista_contactos)
 
 #Decorador para definir la ruta noticias
 @app.route('/noticias')
+#Crea la funcion para llamar a la pagina de noticias
 def noticias():
     return render_template('noticias.html')
 
 #Decorador para definir la ruta sugerencias
 @app.route('/sugerencias')
+#Crea la funcion para llamar a la pagina de sugerencias
 def sugerencias():
     return render_template('sugerencias.html', lista_sugerencias = lista_sugerencias)
 
 #Decorador para definir la ruta reciclaje
 @app.route('/reciclaje')
+#Crea la funcion para llamar a la pagina de reciclaje
 def reciclaje():
     return render_template('reciclaje.html')
 
 #Decorador para definir la ruta Proteccion del medioambiente
 @app.route('/proteccionmedio')
+#Crea la funcion para llamar a la pagina de proteccion medioambiente
 def proteccionmedio():
     return render_template('proteccionmedio.html')
 
 #Decorador para definir la ruta reutilizacion de recursos
 @app.route('/reutilizacionrecurso')
+#Crea la funcion para llamar a la pagina de reutilizacion de recursos
 def reutilizacionrecurso():
     return render_template('reutilizacionrecurso.html')
 
 #Decorador para definir la ruta Responsabilidad social
 @app.route('/responsabilidadsocial')
+#Crea la funcion para llamar a la pagina de responsabilidades sociales
 def responsabilidadsocial():
     return render_template('responsabilidadsocial.html')
 
 #Decorador para definir la ruta Soluciones de almacenamiento
 @app.route('/solucionesalmacenamiento')
+#Crea la funcion para llamar a la pagina de soluciones almacenamiento
 def solucionesalmacenamiento():
     return render_template('solucionesalmacenamiento.html')
 
 #Decorador para definir la ruta de la lista de contactos
 @app.route('/admin')
+#Crea la funcion para llamar a la pagina de administrador
 def admin():
     return render_template('admin.html', lista_contactos = lista_contactos, lista_sugerencias = lista_sugerencias)
 
 #Decorador para definir la ruta Soluciones de almacenamiento
 @app.route('/login')
+#Crea la funcion para llamar a la pagina del login
 def login():
     return render_template('login.html')
 
 #Controlador de la ruta para borrar los datos de la tabla
 @app.route('/ingresar', methods=['POST'])
+#Crea la funcion para el ingreso del login de administrador
 def ingresar():
+    """ Crea la fincion ingresar()
+            Parametros
+            --------------------------------------
+            usuario : str
+                admin@gmail.com
+            contrasenia : str
+                admin
+            login_correo : str
+            login_contrasenia : str
+            
+            Retorna
+            --------------------------------------
+            redireccion : admin
+                redirecciona a la pagina de admin
+            
+            redireccion : login
+                redirecciona a la pagina de login
+        """ 
     usuario = 'admin@gmail.com'
     contrasenia = 'admin'
     if request.method == 'POST':
@@ -83,16 +113,24 @@ def ingresar():
 @app.route('/enviarContacto', methods=['POST']) 
                
 def enviarContacto(): 
-    """ Funcion: enviarContacto() 
-            ingreso_nombre
-            ingreso_apellido
-            ingreso_correo
-            ingreso_telefono
-            ingreso_motivacion
-            -------------------------------------
-            Condicion para no admitir datos nulos
-
-            agrega datos a la lista
+    """ Crea la fincion enviarContacto()
+            Parametros
+            --------------------------------------
+            ingreso_nombre : str
+            ingreso_apellido : str
+            ingreso_correo : str
+            ingreso_telefono : num 
+            ingreso_motivacion : str
+            
+            Retorna
+            --------------------------------------
+            redireccion : contacto
+                redirecciona a la pagina de contacto
+            
+            str
+                envia la informacion a el formulario de destino
+            redireccion : contacto
+                redirecciona a la pagina de contacto
         """                                                      
     if request.method == 'POST':                                            
         ingreso_nombre = request.form['ingreso_nombre']                     
@@ -113,13 +151,19 @@ def enviarContacto():
 #Controlador de la ruta para borrar los datos de la tabla
 @app.route('/borrar', methods=['POST'])
 def borrar():
-    ''' Funcion: borrar()
-            Utiliza el metodo POST
-            -----------------------
-            valida la lita si se encuentra vacia y retorna si realizar cambios
-
-            elimina los datos de la lista
-    '''
+    """ Borra la lista de los contactos()
+            Parametros
+            --------------------------------------
+            lista_contactos : str
+            
+            Retorna
+            --------------------------------------
+            redireccion : admin
+                redirecciona a la pagina de admin
+            
+            clear
+                limpia la lista de contactos
+        """
     if request.method == 'POST':                      
         if lista_contactos == []:                         
             return redirect(url_for('admin'))               
@@ -132,14 +176,22 @@ def borrar():
 @app.route('/enviarSugerencia', methods=['POST']) 
                
 def enviarSugerencia(): 
-    """ Funcion: enviarSugerencia() 
-            ingreso_nombres
-            ingreso_correo_S
-            sugerencias
-            -------------------------------------
-            Condicion para no admitir datos nulos
-
-            agrega datos a la lista
+    """ Crea la fincion enviarSugerencia()
+            Parametros
+            --------------------------------------
+            ingreso_nombres : str
+            ingreso_correo_S : str
+            sugerencias : str
+            
+            Retorna
+            --------------------------------------
+            redireccion : sugerencias
+                redirecciona a la pagina de sugerencias
+            
+            str
+                envia la informacion a el formulario de destino
+            redireccion : sugerencias
+                redirecciona a la pagina de sugerencias
         """                                                      
     if request.method == 'POST':                                            
         ingreso_nombres = request.form['ingreso_nombres']                     
@@ -156,13 +208,19 @@ def enviarSugerencia():
 #Controlador de la ruta para borrar los datos de la tabla
 @app.route('/borrar2', methods=['POST'])
 def borrar2():
-    ''' Funcion: borrar2()
-            Utiliza el metodo POST
-            -----------------------
-            valida la lita si se encuentra vacia y retorna si realizar cambios
-
-            elimina los datos de la lista
-    '''
+    """ Borra la lista de los sugerencias()
+            Parametros
+            --------------------------------------
+            lista_sugerencias : str
+            
+            Retorna
+            --------------------------------------
+            redireccion : admin
+                redirecciona a la pagina de admin
+            
+            clear
+                limpia la lista de contactos
+        """
     if request.method == 'POST':                      
         if lista_sugerencias == []:                         
             return redirect(url_for('admin'))               
